922ed4fc1aaa62291c6ef6fdaddad3983dc6aa3a
muun-y <munyoungc31@gmail.com>
2023-03-19 23:12:02 -0700

    add logout function to navbar

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,7 +1,7 @@
-//---------------------------------------------------
-// This function loads the parts of your skeleton 
-// (navbar, footer, and other things) into html doc. 
-//---------------------------------------------------
+// //---------------------------------------------------
+// // This function loads the parts of your skeleton 
+// // (navbar, footer, and other things) into html doc. 
+// //---------------------------------------------------
 function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
@@ -12,9 +12,22 @@ function loadSkeleton() {
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         } else {
             // No user is signed in.
-            console.log($('#navbarPlaceholder').load('./text/navbar.html'));
+            console.log($('#navbarPlaceholder').load('./text/nav_before_login.html'));
             console.log($('#footerPlaceholder').load('./text/footer.html'));
         }
     });
 }
 loadSkeleton(); //invoke the function
+
+
+//invoke the function
+function logout() {
+    firebase.auth().signOut().then(() => {
+        // Sign-out successful.
+        console.log("logging out user");
+        window.location.href="home.html";
+      }).catch((error) => {
+        // An error happened.
+      });
+  }
+
