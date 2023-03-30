bd4fa6a5e923a7b2be56d43e98c9db8931b0deee
Kenne12345 <kennethahn@hotmail.com>
2023-03-30 09:48:13 -0700

    Merge branch 'main' of https://github.com/Martxian/1800_202310_BBY11

---
+++ scripts/weather.js
@@ -0,0 +1,11 @@
+// Current Weather API Endpoint
+
+http://api.weatherstack.com/current
+    ? access_key = 115ae663acb91c7d645b770b736a5a42
+    & query = Surrey
+    
+// optional parameters: 
+
+    & units = m
+    & language = en
+    & callback = MY_CALLBACK
