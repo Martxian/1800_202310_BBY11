5863d75e94564a3ca5f8dd7b99b025ab6d58346f
Marvin Sio <marvinbcit@gmail.com>
2023-04-03 00:50:26 -0700

    changed default reference

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,96 +1,96 @@
 function populateUserInfo() {
-  firebase.auth().onAuthStateChanged((user) => {
-    // Check if user is signed in:
-    if (user) {
-      //go to the correct user document by referencing to the user uid
-      currentUser = db.collection("users").doc(user.uid);
-      //get the document for current user.
-      currentUser.get().then((userDoc) => {
-        //get the data fields of the user
-        var email = userDoc.data().email;
-        var userName = userDoc.data().name;
-        var displayAddress = userDoc.data().addess;
-        var userAboutMe = userDoc.data().aboutme;
-        let picUrl = userDoc.data().profilePic;
+    firebase.auth().onAuthStateChanged((user) => {
+        // Check if user is signed in:
+        if (user) {
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid);
+            //get the document for current user.
+            currentUser.get().then((userDoc) => {
+                //get the data fields of the user
+                var email = userDoc.data().email;
+                var userName = userDoc.data().name;
+                var displayAddress = userDoc.data().addess;
+                var userAboutMe = userDoc.data().aboutme;
+                let picUrl = userDoc.data().profilePic;
 
         //if the data fields are not empty, then write them in to the form.
-        if (email != null) {
-          document.getElementById("emailInput").value = email;
+                if (email != null) {
+                    document.getElementById("emailInput").value = email;
+                }
+                if (userName != null) {
+                    document.getElementById("nameInput").value = userName;
+                }
+                if (displayAddress != null) {
+                    document.getElementById("displayAddress").value = displayAddress;
+                }
+                if (userAboutMe != null) {
+                    document.getElementById("aboutMeInput").value = userAboutMe;
+                }
+                if (picUrl != null) {
+                    $("#mypic-goes-here").attr("src", picUrl);
+                } else $("#mypic-goes-here").attr("src", "../images/default.jpg");
+            });
+        } else {
+            // No user is signed in.
+            console.log("No user is signed in");
+            alert("Please Sign-in to view your Profile.");
         }
-        if (userName != null) {
-          document.getElementById("nameInput").value = userName;
-        }
-        if (displayAddress != null) {
-          document.getElementById("displayAddress").value = displayAddress;
-        }
-        if (userAboutMe != null) {
-          document.getElementById("aboutMeInput").value = userAboutMe;
-        }
-        if (picUrl != null) {
-          $("#mypic-goes-here").attr("src", picUrl);
-        } else $("#mypic-goes-here").attr("src", "../images/profilepic.png");
-      });
-    } else {
-      // No user is signed in.
-      console.log("No user is signed in");
-      alert("Please Sign-in to view your Profile.");
-    }
-  });
+    });
 }
 //call the function to run it
 populateUserInfo();
 
 const editButton = document.getElementById("edit-button");
 editButton.addEventListener("click", function editUserInfo() {
-  //Enable the form fields
-  document.getElementById("personalInfoFields").disabled = false;
+    //Enable the form fields
+    document.getElementById("personalInfoFields").disabled = false;
 });
 
 var ImageFile; //global variable to store the File Object reference
 
 function chooseFileListener() {
-  const fileInput = document.getElementById("mypic-input"); // pointer #1
-  const image = document.getElementById("mypic-goes-here"); // pointer #2
+    const fileInput = document.getElementById("mypic-input"); // pointer #1
+    const image = document.getElementById("mypic-goes-here"); // pointer #2
 
-  //when this file changes, do something
-  fileInput.addEventListener("change", function (e) {
-    //the change event returns a file "e.target.files[0]"
-    ImageFile = e.target.files[0];
-    var blob = URL.createObjectURL(ImageFile);
+    //when this file changes, do something
+    fileInput.addEventListener("change", function (e) {
+        //the change event returns a file "e.target.files[0]"
+        ImageFile = e.target.files[0];
+        var blob = URL.createObjectURL(ImageFile);
 
-    image.src = blob; //assign the "src" property of the "img" tag
-  });
+        image.src = blob; //assign the "src" property of the "img" tag
+    });
 }
 chooseFileListener();
 
 function saveUserInfo() {
-  firebase.auth().onAuthStateChanged(function (user) {
-    var storage = firebase.storage();
-    var storageRef = storage.ref("images/" + user.uid + ".jpg");
+    firebase.auth().onAuthStateChanged(function (user) {
+        var storage = firebase.storage();
+        var storageRef = storage.ref("images/" + user.uid + ".jpg");
 
-    //Asynch call to put File Object (global variable ImageFile) onto Cloud
-    storageRef.put(ImageFile).then(function () {
-      //Asynch call to get URL from Cloud
-      storageRef.getDownloadURL().then(function (url) {
-        userName = document.getElementById("nameInput").value;
-        displayAddress = document.getElementById("displayAddress").value;
-        userAboutMe = document.getElementById("aboutMeInput").value;
-        //Asynch call to save the form fields into Firestore.
-        db.collection("users")
-          .doc(user.uid)
-          .update({
-            name: userName,
-            displayAddress: displayAddress,
-            aboutme: userAboutMe,
-            profilePic: url, // Save the URL into users collection
-          })
-          .then(function () {
-            console.log("Saved user profile info");
-            alert("Your Profile has been updated!");
-          });
-        document.getElementById("personalInfoFields").disabled = true;
-        document.getElementById("mypic-input").value = "";
-      });
+        //Asynch call to put File Object (global variable ImageFile) onto Cloud
+        storageRef.put(ImageFile).then(function () {
+            //Asynch call to get URL from Cloud
+            storageRef.getDownloadURL().then(function (url) {
+                userName = document.getElementById("nameInput").value;
+                displayAddress = document.getElementById("displayAddress").value;
+                userAboutMe = document.getElementById("aboutMeInput").value;
+                //Asynch call to save the form fields into Firestore.
+                db.collection("users")
+                    .doc(user.uid)
+                    .update({
+                        name: userName,
+                        displayAddress: displayAddress,
+                        aboutme: userAboutMe,
+                        profilePic: url, // Save the URL into users collection
+                    })
+                    .then(function () {
+                        console.log("Saved user profile info");
+                        alert("Your Profile has been updated!");
+                    });
+                document.getElementById("personalInfoFields").disabled = true;
+                document.getElementById("mypic-input").value = "";
+            });
+        });
     });
-  });
 }
